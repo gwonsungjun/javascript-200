a79025538292c006536116d78a7784b1c89a36a3
Sungjun <sungjunpizz@gmail.com>
2019-04-09 22:32:34 +0900

    Add part2

---
+++ examples/part2/019.js
@@ -0,0 +1,6 @@
+var value = null;
+console.log(value);
+console.log(typeof value);
+var val;
+console.log(val);
+console.log(typeof val);
---
+++ examples/part2/032.js
@@ -0,0 +1,10 @@
+var obj = {a: 1, b: 2, c: 30, d: 44, e: 5};
+
+var {a, c} = obj;
+console.log(`a >>> ${a}`);
+console.log(`c >>> ${c}`);
+
+var {a:newA=10, f:newF=5} = obj;
+console.log(`new A >>> ${newA}`);
+console.log(`new F >>> ${newF}`);
+console.log(obj);
