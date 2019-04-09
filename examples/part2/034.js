c6f3bc9f4f4f5dddc6ec85862490d6c42b498fa4
Sungjun <sungjunpizz@gmail.com>
2019-04-09 22:54:37 +0900

    Add 034, 035, 035-1, 036 js file

---
+++ examples/part2/034.js
@@ -0,0 +1,10 @@
+var greeting_expression = function(name) {
+    console.log('Hi, ' + name);
+}
+
+function gretting_declaration(name) {
+    console.log('Hi, ' + name);
+}
+
+greeting_expression('Chloe');
+gretting_declaration('Chloe');
