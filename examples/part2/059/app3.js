af6545ee93b05592f876c19492886bac84274c9d
Sungjun <sungjunpizz@gmail.com>
2019-05-05 23:28:05 +0900

    Add 063-1 ~ 067

--- examples/part2/059/app3.js
+++ examples/part2/059/app3.js
@@ -3,3 +3,4 @@ import {value} from './value.js';
 console.log(value);
 
 setTimeout(() => console.log(value), 2000);
+
---
+++ examples/part3/063-1.js
@@ -0,0 +1,8 @@
+function verifyinteger(n) {
+    if(!Number.isInteger(n)) return 0;
+    return n;
+}
+const num1 = verifyinteger(15);
+const num2 = verifyinteger(Infinity);
+const num3 = verifyinteger(0.05);
+console.log(num1, num2, num3);
